d1fb15992278572fb03c0b8656bc4ed5884956a4
Cassandra1539 <cassandra.hjortdahl@nmbu.no>
2024-05-07 09:56:46 +0200

    Add CAMISIM input files

---
+++ CAMISIM_input/50_10x_scripts/gr2_a.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --account=p1068_tsd
+#SBATCH --job-name=CAMISIM_50_10x_gr2_a
+#SBATCH --nodes=1
+#SBATCH --time=7-00:00:00
+#SBATCH --mem-per-cpu=20GB
+#SBATCH --output=output_%j.txt
+#SBATCH --error=error_%j.txt
+
+module --quiet purge  # Reset the modules to the system default
+
+module load matplotlib/3.4.3-foss-2021b
+module load Biopython/1.79-foss-2021b
+module load biom-format/2.1.12-foss-2021b
+module load scikit-learn/1.0.1-foss-2021b
+module load SAMtools/1.14-GCC-11.2.0
+module load Perl/5.34.0-GCCcore-11.2.0
+
+cd /PATH/CAMISIM/
+
+python metagenomesimulation.py ./config_files/config_50_10x_gr2_a.ini
---
+++ CAMISIM_input/slurm_scripts_de_novo/camisim_pilot_10_rep.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --account=p1068_tsd
+#SBATCH --job-name=CAMISIM_pilot_10_rep
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=20:00:00
+#SBATCH --mem-per-cpu=10GB
+#SBATCH --output=output_%j.txt
+#SBATCH --error=error_%j.txt
+#SBATCH --cpus-per-task=8
+
+module --quiet purge  # Reset the modules to the system default
+
+module load matplotlib/3.4.3-foss-2021b
+module load Biopython/1.79-foss-2021b
+module load biom-format/2.1.12-foss-2021b
+module load scikit-learn/1.0.1-foss-2021b
+module load SAMtools/1.14-GCC-11.2.0
+module load Perl/5.34.0-GCCcore-11.2.0
+
+cd /PATH/CAMISIM/
+
+python metagenomesimulation.py ./config_files/config_pilot_10_rep.ini 
